75704f0140c54eb00fe61873dc6c1243dfa6e630
Matthew Mata <matthewmata1030@gmail.com>
2020-01-08 00:53:48 -0800

    finished

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-
+// Completed
 
 
 ReactDOM.render(<App first={'Matthew'} last={'Mata'}/>, document.getElementById('app'));
